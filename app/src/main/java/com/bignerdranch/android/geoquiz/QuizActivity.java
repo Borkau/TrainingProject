a068b3e01d9b4a725eb308a183b5eb55a61bc78a
Borkau <borkau.box@gmail.com>
2020-08-05 04:06:58 +0500

    Add function of percent true answers

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -21,34 +21,29 @@ public class QuizActivity extends AppCompatActivity {
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
 
-    private Question[] mQuestionBank = new Question[] {
-      new Question(R.string.question_australia, true),
-      new Question(R.string.question_oceans, true),
-      new Question(R.string.question_mideast, false),
-      new Question(R.string.question_africa, false),
-      new Question(R.string.question_americas, true),
-      new Question(R.string.question_asia, true),
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
     };
 
     private int mCurrentIndex = 0;
+    private float mRightAnswer = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
-        if(savedInstanceState != null) {
+        if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -84,13 +79,13 @@ public class QuizActivity extends AppCompatActivity {
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mCurrentIndex == 0) {
+                if (mCurrentIndex == 0) {
                     mCurrentIndex = mQuestionBank.length - 1;
                 }
                 mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
                 updateQuestion();
-           }
-          });
+            }
+        });
         updateQuestion();
     }
 
@@ -99,27 +94,32 @@ public class QuizActivity extends AppCompatActivity {
         super.onStart();
         Log.d(TAG, "onStart() called");
     }
+
     @Override
     public void onResume() {
         super.onResume();
         Log.d(TAG, "onResume() called");
     }
+
     @Override
     public void onPause() {
         super.onPause();
         Log.d(TAG, "onPause() called");
     }
+
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
+
     @Override
     public void onStop() {
         super.onStop();
         Log.d(TAG, "onStop() called");
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -127,24 +127,29 @@ public class QuizActivity extends AppCompatActivity {
     }
 
 
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+        mTrueButton.setEnabled(true);
+        mFalseButton.setEnabled(true);
+    }
 
-        private void updateQuestion() {
-            int question = mQuestionBank[mCurrentIndex].getTextResId();
-            mQuestionTextView.setText(question);
-            mTrueButton.setEnabled(true);
-            mFalseButton.setEnabled(true);
-            }
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
-        private void checkAnswer(boolean userPressedTrue) {
-            boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-
-            int messageResId = 0;
-            if(userPressedTrue == answerIsTrue) {
-                messageResId = R.string.correct_toast;
-            } else {
-                messageResId = R.string.incorrect_toast;
-            }
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+            mRightAnswer++;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        if(mCurrentIndex < 5) {
             Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+        } else {
+            int mPercentRight = Math.round((mRightAnswer / 6) * 100);
+            Toast.makeText(this, Integer.toString(mPercentRight) + "% right answers", Toast.LENGTH_LONG).show();
+        }
     }
 }
 
